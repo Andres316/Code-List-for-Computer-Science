bcc5915387a9adc093e296748848d4502ee65b31
Andres316 <34690998+Andres316@users.noreply.github.com>
2018-01-09 14:40:46 -0500

    Add files via upload

---
+++ File 1-9-18.py
@@ -0,0 +1,38 @@
+Python 3.5.2 (v3.5.2:4def2a2901a5, Jun 26 2016, 10:47:25) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
+Visit http://www.python.org/download/mac/tcltk/ for current information.
+"hello"=='hello'
+True
+>>> eggCount=42
+>>> eggCount<=42
+True
+>>> myAge=17
+>>> myAge>=30
+False
+>>> 17>30
+False
+>>> (4<5)and (5<6)
+True
+>>> age= 29
+>>> if age==29:
+       print("You are 29")
+print("Welcome to Gmail")
+SyntaxError: invalid syntax
+>>> print("Welcome to Gmail")
+Welcome to Gmail
+>>> my username= input("Enter your username and then press Return: ")
+SyntaxError: invalid syntax
+>>> myUsername =input("Enter your username, then press Return: ")
+Enter your username, then press Return: Andy316
+>>> if myUsername=="Andy316":
+	print("Username succesful")
+	myPassword=input("Enter your password, then press Return: ")
+	Andy316
+if myPassword=="sof1230":
+	
+SyntaxError: invalid syntax
+>>> if myPassword=="sof123@"
+SyntaxError: invalid syntax
+>>> 
