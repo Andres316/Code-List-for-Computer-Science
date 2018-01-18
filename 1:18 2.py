ff2429f009f2323ad4f26cb0fa634c18d1aba3aa
Andres316 <34690998+Andres316@users.noreply.github.com>
2018-01-18 14:27:23 -0500

    Add files via upload

--- 1:18 2.py
+++ 1:18 2.py
@@ -1,23 +1,6 @@
-name=""
-while name != 'your name':
-    print('Please type your name.')
-    name=input()
-print("Thank you!")
+x=int(input("Enter your age: "))
+if x<18:
+   print("You are not able to vote")
+else:
+   print("You are able to vote")
 
-
-while True:
-    print('Please type your name.')
-    name = input()
-    if name == 'your name':
-        break
-
-while True:
- print('Enter your username?')
- name = input()
- if name != 'Joe':
-   continue
- print('Hello, Joe.What is the password? (It is a fish)')
- password = input()
- if password =='swordfish':
-     break
- print('Access granted.') 
