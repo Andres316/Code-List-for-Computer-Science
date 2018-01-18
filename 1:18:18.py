1bbd00c1ed43a1ccf933de4198ac20ca406dfa1b
Andres316 <34690998+Andres316@users.noreply.github.com>
2018-01-18 13:37:32 -0500

    Add files via upload

---
+++ 1:18:18.py
@@ -0,0 +1,127 @@
+Python 3.5.1 (v3.5.1:37a07cee5969, Dec  5 2015, 21:12:44) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
+Visit http://www.python.org/download/mac/tcltk/ for current information.
+Float 3.12
+SyntaxError: invalid syntax
+>>> String 'mike'
+SyntaxError: invalid syntax
+>>> Integer 12
+SyntaxError: invalid syntax
+>>> Boolean True
+SyntaxError: invalid syntax
+>>> 4 < 5
+True
+>>> 5 < 6
+True
+>>> 
+================ RESTART: /Users/Guest/Documents/Untitled.py ================
+5050
+>>> 
+================ RESTART: /Users/Guest/Documents/Untitled.py ================
+5050
+>>> 
+================ RESTART: /Users/Guest/Documents/Untitled.py ================
+5050
+>>> 
+================ RESTART: /Users/Guest/Documents/Untitled.py ================
+4950
+>>> total=0
+for num in range(100):
+    total=total+num
+print(total)
+SyntaxError: multiple statements found while compiling a single statement
+>>> total= 0
+for num in range(100):
+    total=total+num
+print(total)
+SyntaxError: multiple statements found while compiling a single statement
+>>> 'hello' == 'Hello'
+False
+>>> True! = False
+SyntaxError: invalid syntax
+>>> True != False
+True
+>>> 42 == '42'
+False
+>>> myAge=29
+>>> myAge>=15
+True
+>>> myAge==15
+False
+>>> 
+================ RESTART: /Users/Guest/Documents/Untitled.py ================
+4950
+0 is an even number
+2 is an even number
+4 is an even number
+6 is an even number
+8 is an even number
+10 is an even number
+12 is an even number
+14 is an even number
+16 is an even number
+18 is an even number
+20 is an even number
+22 is an even number
+24 is an even number
+26 is an even number
+28 is an even number
+16 is an even number
+18 is an even number
+20 is an even number
+22 is an even number
+24 is an even number
+26 is an even number
+28 is an even number
+>>> total=0
+for num in range(100):
+    total=total+num
+print(total)
+
+for number in range(30):
+    if number%2 == 0:
+        print(number, "is an even number")
+
+for number in range(15,30):
+    if number%2 == 0:
+        print(number, "is an even number")
+        
+SyntaxError: multiple statements found while compiling a single statement
+>>> total=0
+for num in range(100):
+    total=total+num
+print(total)
+SyntaxError: multiple statements found while compiling a single statement
+>>> total= 0
+for num in range(100):
+    total=total+num
+print(total)
+SyntaxError: multiple statements found while compiling a single statement
+>>> 'hello' == 'Hello'
+False
+>>> True! = False
+SyntaxError: invalid syntax
+>>> True != False
+True
+>>> 42 == '42'
+False
+>>> myAge=29
+>>> myAge>=15
+True
+>>> myAge==15
+False
+SyntaxError: multiple statements found while compiling a single statement
+>>> Float 3.12
+SyntaxError: invalid syntax
+>>> String 'mike'
+SyntaxError: invalid syntax
+>>> Integer 12
+SyntaxError: invalid syntax
+>>> Boolean True
+SyntaxError: invalid syntax
+>>> 4 < 5
+True
+>>> 5 < 6
+True
