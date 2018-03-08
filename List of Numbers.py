91c9ec848f47a6d1e3b2c5f10195929e25eda632
Andres316 <34690998+Andres316@users.noreply.github.com>
2018-03-08 14:19:38 -0500

    Add files via upload

---
+++ List of Numbers.py
@@ -0,0 +1,19 @@
+import random
+mylist = []
+
+mylist.append(95)
+mylist.append(87)
+mylist.append(77)
+mylist.append(100)
+print(mylist)
+
+# reversing a list
+mylist.reverse()
+print(mylist)
+# sort the list
+
+mylist.sort()
+print(mylist)
+
+random.shuffle(mylist)
+print(mylist)
