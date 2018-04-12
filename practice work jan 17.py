8096cd8ae73cb8f08c02fb2adbd1a6547ebb561d
Andres316 <34690998+Andres316@users.noreply.github.com>
2018-04-12 13:30:44 -0400

    Add files via upload

---
+++ practice work jan 17.py
@@ -0,0 +1,28 @@
+'''
+first = input("Ener your first name: ")
+last = input("Enter your last name: ")
+
+name = first[0]  + " " + last
+print("Welcome to our game" , name)
+
+# String functions
+
+a = "abra"
+b = "cad"
+
+print(a,b,a)
+
+# String functions
+
+nums = [5, 10, 15, 20, 25, 30, 35, 40]
+
+def maths():
+    print("Number plus Number")
+    print(nums[7] - nums[1])
+
+print(maths())
+'''
+
+x = 2.5
+y = -1.5
+m = 18
