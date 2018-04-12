d47aad4f52abc641ff388b361400acf4700039a9
Andres316 <34690998+Andres316@users.noreply.github.com>
2018-04-12 13:31:06 -0400

    Add files via upload

---
+++ Lottery.py
@@ -0,0 +1,14 @@
+# processing and manipulating lists
+
+import random
+
+lotteryNumbers=[]
+
+lotteryNumbers.append(random.randint(1,70))
+lotteryNumbers.append(random.randint(1,70))
+lotteryNumbers.append(random.randint(1,70))
+lotteryNumbers.append(random.randint(1,70))
+lotteryNumbers.append(random.randint(1,70))
+lotteryNumbers.append(random.randint(1,70))
+
+print(lotteryNumbers)
