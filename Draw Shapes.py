350b41034d93386eaed60c18cbd36af4850f91a8
Andres316 <34690998+Andres316@users.noreply.github.com>
2018-05-10 14:46:02 -0400

    Add files via upload

---
+++ Draw Shapes.py
@@ -0,0 +1,17 @@
+from Shapes import Triangle, Rectangle, Oval
+
+rectangle1=Rectangle()
+
+rectangle1.set_width(200)
+rectangle1.set_height(100)
+rectangle1.set_color('red')
+
+rectangle1.draw()
+
+new_rect=Rectangle()
+
+new_rect.set_width(70)
+new_rect.set_height(50)
+new_rect.set_color('blue')
+
+new_rect.draw()
